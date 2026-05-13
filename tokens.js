942d8563fc040dd2f7652a332f7f3cf8e8d58fbc
Afganli97 <113414023+Afganli97@users.noreply.github.com>
2026-05-13 21:52:10 +0400

    Add token data for multiple chains

---
+++ tokens.js
@@ -0,0 +1,308 @@
+module.exports = [
+  {
+    name: "andy",
+    chain: "ethereum",
+    address: "0x68BbEd6A47194EFf1CF514B50Ea91895597fc91E",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "wolf",
+    chain: "ethereum",
+    address: "0x67466BE17df832165F8C80a5A120CCc652bD7E69",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "brett",
+    chain: "ethereum",
+    address: "0x66bFf695f3B16a824869a8018a3A6e3685241269",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "boys",
+    chain: "ethereum",
+    address: "0x706987611C5d2052541D64ef8f036916807c916A",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "girls",
+    chain: "ethereum",
+    address: "0x69420bad4e6F844C023488fFd44433626602bD69",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "spike",
+    chain: "ethereum",
+    address: "0x19848077f45356b21164c412Eff3D3E4ff6Ebc31",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "birddog",
+    chain: "ethereum",
+    address: "0xF6Ce4BE313EaD51511215F1874c898239A331E37",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "draggy",
+    chain: "ethereum",
+    address: "0xd12A99dbC40036CEc6f1b776dccd2d36f5953B94",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "doggy",
+    chain: "ethereum",
+    address: "0x09d92c109b475Dd513292C76544B4E250dA13faA",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "gondola",
+    chain: "ethereum",
+    address: "0xd43fbA1f38d9B306AEEF9d78aD177d51Ef802B46",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "punk",
+    chain: "ethereum",
+    address: "0xcec8314cf4d448fbd3525696af045f13ccea7444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "gop",
+    chain: "ethereum",
+    address: "0x80810a9c31e7243a0bfb9919b0b4020378d1c134",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "e",
+    chain: "ethereum",
+    address: "0xc0cfbe1602dd586349f60e4681bf4badca584ec9",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "morti",
+    chain: "ethereum",
+    address: "0x6969e5cfe7578ac5f06d313c1a25578927a5bbc9",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "crazy",
+    chain: "ethereum",
+    address: "0xad5fdc8c3c18d50315331fca7f66efe5033f6c4c",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "chillguy",
+    chain: "ethereum",
+    address: "0x60215db40b04fe029c42c56ff2e02221c1f288ef",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "dogs",
+    chain: "ethereum",
+    address: "0xf9962329a1e9fa84836d2f37640d7012fb588e9d",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "desu",
+    chain: "ethereum",
+    address: "0xb40865a6ed718f57468cd3f4f60825a130b89a51",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "dope",
+    chain: "ethereum",
+    address: "0x43c5034469bce262d32f64c5e7f9f359f5b1495f",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "chwy",
+    chain: "ethereum",
+    address: "0xce176825afc335d9759cb4e323ee8b31891de747",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "dogecast",
+    chain: "ethereum",
+    address: "0x4955f6641bf9c8c163604c321f4b36e988698f75",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "dankdoge",
+    chain: "ethereum",
+    address: "0x6f0066f9df5abf780cfc69ebe50b71fc4cdb08b0",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "scam",
+    chain: "ethereum",
+    address: "0xefc4a88dd07e43436344548e786602b0f2631110",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "bsc",
+    chain: "bsc",
+    address: "0x6331bf8d601f0d7f0d2101772af5137c418c4444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "priceless",
+    chain: "bsc",
+    address: "0x7d03759e5b41e36899833cb2e008455d69a24444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "palu",
+    chain: "bsc",
+    address: "0x02e75d28a8aa2a0033b8cf866fcf0bb0e1ee4444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "pup",
+    chain: "bsc",
+    address: "0x73b84f7e3901f39fc29f3704a03126d317ab4444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "broccoli",
+    chain: "bsc",
+    address: "0x12b4356c65340fb02cdff01293f95febb1512f3b",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "szn",
+    chain: "bsc",
+    address: "0x23b35c7f686cac8297ea6e81a467286481ca4444",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "bnbxbt",
+    chain: "bsc",
+    address: "0xa18bbdcd86e4178d10ecd9316667cfe4c4aa8717",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "1",
+    chain: "solana",
+    address: "GMvCfcZg8YvkkQmwDaAzCtHDrrEtgE74nQpQ7xNabonk",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "nothing",
+    chain: "solana",
+    address: "F7pB3ZdfBnyFw2LRHydWEn9BmhEa5XihXLjhySFRpump",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "valor",
+    chain: "solana",
+    address: "3wPQhXYqy861Nhoc4bahtpf7G3e89XCLfZ67ptEfZUSA",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  },
+  {
+    name: "nai",
+    chain: "solana",
+    address: "Fxm6tJ2khHbLVcSgnYUqg9FiBduVRLEvdQR67dvQ3eGp",
+    alertAbove: null,
+    alertBelow: null,
+    changeAlert: 10,
+    swapAlert: null
+  }
+];
